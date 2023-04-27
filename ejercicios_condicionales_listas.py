a9e9712826208c9f247dbaa26741e914272d19f4
Cecilia Rey <97458514+Ceciliarey95@users.noreply.github.com>
2023-04-27 19:23:31 -0300

    Ejercicios

---
+++ ejercicios_condicionales_listas.py
@@ -0,0 +1,63 @@
+#Condicionales:
+'''1_Crear una lista de números con 5 números ingresados por el usuario. Luego crea una lista 
+que contenga solo los 3 últimos números de la lista original.'''
+#Para cortar desde inicio hasta fin, se usa esto:
+#lista[inicio:fin]
+#Por ejemplo, si es cero, no es necesario que se especifique:
+#lista[:2]
+#También se puede cortar desde determinado lugar hasta el final:
+#lista[2:]
+
+'''2_Determinar si un alumno aprueba a reprueba un curso, sabiendo que aprobara 
+si su promedio de tres calificaciones es mayor o igual a 70; desaprueba en caso contrario.'''
+
+'''3_Hacer un programa que imprima el nombre de un artículo, clave, precio original y su precio
+ con descuento. El descuento lo hace en base a la clave, si la clave es 01 el descuento es del 10% 
+ y si la clave es 02 el descuento en del 20% (solo existen dos claves). Los datos se ingresan por consola 
+ (nombre,clave y precio) ;)'''
+
+'''4_Un comercio ofrece un descuento del 15% sobre el total de la compra si esta supera los $1000. 
+Obtenga para determinado cliente cuánto deberá pagar finalmente por su compra y el descuento obtenido, 
+si es que corresponde. P/D: Ingresá el total por consola'''
+
+'''5_Realizar un programa que sea capaz de, habiéndose ingresado dos números m y n, determine si n es 
+divisor de m.'''
+
+'''6_Tres personas deciden invertir su dinero para fundar una empresa. Cada una de ellas invierte una
+cantidad distinta.Obtener el porcentaje que cada quien invierte con respecto a la cantidad total invertida 
+e imprimilos.(ingresá las inversiones por consola)'''
+
+'''7_Leer 2 números; si son iguales que los multiplique, si el primero es mayor que el segundo que los 
+reste y si no que los sume.
+'''
+#Listas, tuplas y diccionarios:
+'''1_Haz un programa que almacene 5 elementos en una variable del tipo lista, la modiﬁque para que cada 
+componente sea igual al cuadrado del componente original. El programa mostrara la lista resultante por pantalla.'''
+
+'''2_Realice una función que dada una lista de enteros L, y un número entero n(que se ingresa). Elimine de la 
+lista original el elemento que sea igual a ese número dado e imprime la lista sin ese número'''
+
+'''3_Escriba un algoritmo que permita cargar 5 números y agregarlos a una lista. Y que luego, una vez cargada,
+ controle y sustituya cualquier elemento negativo por 0.'''
+
+'''4_Cargue dos listas, y actualice la primer lista con los elementos que están en la segunda.'''
+
+'''5_Crea una tupla con los meses del año, pide números al usuario, si el numero esta entre 1 y la longitud
+máxima de la tupla, muestra el contenido de esa posición sino muestra un mensaje de error.'''
+
+'''6_Crea una tupla con números, pide un numero por teclado e indica cuantas veces se repite.'''
+
+'''7_Crea un diccionario que simula ser una Agenda Telefonica y consulta un dato con su clave'''
+
+'''8_Escribir un programa que guarde en una variable un diccionario con 3 tipos de divisas(su nombre y el simbolo), 
+pregunte al usuario por una divisa y muestre su símbolo o un mensaje de aviso si la divisa no está en el diccionario.'''
+
+'''9_Escribir un programa que pregunte al usuario su nombre, edad, dirección y teléfono y lo guarde en un diccionario. 
+Después debe mostrar por pantalla el mensaje: <nombre> tiene <edad> años, vive en <dirección> y su número de teléfono es <teléfono>.'''
+
+'''10_Escribir un programa que guarde en un diccionario los precios de 5 frutas distintas, pregunte al usuario por una fruta, 
+un número de kilos y muestre por pantalla el precio de ese número de kilos de fruta. Si la fruta no está en el diccionario 
+debe mostrar un mensaje informando de ello.'''
+
+'''11_Escribir un programa que cree un diccionario simulando una cesta de la compra. El programa debe preguntar 5 artículos 
+y sus precios y añadir el par al diccionario. Después se debe mostrar por pantalla la lista de la compra y el monto total'''
